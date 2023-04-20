c7be4e8ebe05917dae878c58e7dae126fbdbb9d1
Gaurav Rai <grv.rai@live.in>
2023-04-20 16:15:04 +0530

    Update strategy.py

--- backtrader/strategy.py
+++ backtrader/strategy.py
@@ -526,8 +526,7 @@ class Strategy(with_metaclass(MetaStrategy, StrategyBase)):
             trade = datatrades[-1]
 
         for exbit in order.executed.iterpending():
-            print('exbit: %s' % exbit)
-            print(exbit.opened, exbit.closed, exbit.price, exbit.value, exbit.comm)
+
             if exbit is None:
                 break
 
